d9d9d211b01f3725ced2c91fb380e3ef410361e5
Justin Kang <j6k@Justins-MacBook-Pro.local>
2017-11-07 13:56:20 -0800

    [ops] - test commit

--- socket-server/src/index.js
+++ socket-server/src/index.js
@@ -1,10 +1,23 @@
-import express from 'express';
+import http from 'http';
+import SocketIO from 'socket.io';
 
 import { success } from './lib/log';
 
-const app = express();
+const server = http.createServer();
+const io = SocketIO(server);
+
+io.on('connection', (client) => {
+  client.on('message', (msg) => {
+    success('message event heard');
+    client.to().emit('message', msg);
+  });
+  client.on('video-chat', (video) => {
+    success('video-chat even heard');
+    client.to().emit('video-chat', video);
+  });
+});
 
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   success('socket-server listening on port ', PORT);
 });
