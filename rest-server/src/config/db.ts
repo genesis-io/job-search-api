0e5b406179a0b7aaa39e0d03b1c6e5b910d56f5c
Justin Kang <jmkang06@gmail.com>
2017-12-11 21:41:14 -0800

    [ops] - .env usage in db config, sample env

--- rest-server/src/config/db.ts
+++ rest-server/src/config/db.ts
@@ -15,9 +15,9 @@ const options: Option = {
 }
 
 const dataBase: any = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: options.db
 });
 
