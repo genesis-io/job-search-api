82c088ccf8a16dc344f56e44231db837ea82c485
Justin Kang <jmkang06@gmail.com>
2018-02-08 14:03:58 -0800

    [ops] - projects psql changes and globals - using global controllers and queries - test reflecting psql changes - type check on sql helpers

--- rest-server/src/components/project/projectQueries.ts
+++ rest-server/src/components/project/projectQueries.ts
@@ -1,27 +1,13 @@
-import dataBase from '../../config/db';
-
+import { globalQueryHelper } from '../../lib/components/globals';
 import {
-  getProjects,
-  postProject
+  getProjectsHelper,
+  postProjectHelper
 } from './projectSQLHelpers';
-import { error } from '../../lib/log';
 
-export const findProjects = async (userId: number) => {
-  try {
-    const queryString = getProjects(userId);
-    const result = dataBase.queryAsync(queryString);
-    return result;
-  } catch (e) {
-    error(e);
+export const projectQuery = async (payload, url) => {
+  if (url === '/') {
+    return await globalQueryHelper(payload, postProjectHelper, 'postProjectHelper');
+  } else {
+    return await globalQueryHelper(payload, getProjectsHelper, 'getProjectsHelper');
   }
-}
-
-export const saveProject = async (title: string, description: string, collaborators: string, userId: number) => {
-  try {
-    const queryString = postProject(title, description, collaborators, userId);
-    const result = dataBase.queryAsync(queryString);
-    return result;
-  } catch (e) {
-    error(e);
-  }
-}
+};
--- rest-server/src/components/project/projectSQLHelpers.ts
+++ rest-server/src/components/project/projectSQLHelpers.ts
@@ -1,13 +1,15 @@
-export const getProjects = (userId: number) => {
+export const getProjectsHelper = ({ userId }: { userId: number }): string => {
   return `
-    SELECt * FROM projects
+    SELECt id, title, description, collaborators, user_id 
+    FROM projects
     WHERE user_id=${userId}
-  `
+  `;
 };
 
-export const postProject = (title: string, description: string, collaborators: string, userId: number) => {
+export const postProjectHelper = ({ title, description, collaborators, userId }: { title: string, description: string, collaborators: string, userId: number }): string => {
   return `
     INSERT INTO projects (title, description, collaborators, user_id)
     VALUES ('${title}', '${description}', '${collaborators}', ${userId})
-  `
+    RETURNING id, title, description, collaborators, user_id
+  `;
 };
